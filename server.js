939cde01b6658d3b9f7e828f1c82f6718bae4bbf
galliniaj-byte <galliniaj@vcu.edu>
2026-04-28 14:47:12 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -4,10 +4,16 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const greeting = process.env.GREETING || "Hello from your deployed app!";
+
 app.use(express.static(__dirname));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "proj3.html"));
+});
+
+app.get("/api/message", (req, res) => {
+  res.json({ message: greeting });
 });
 
 app.listen(PORT, () => {
